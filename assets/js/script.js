a10c09837f49fcfd230474f086715cac244c20b6
Felipe de Lima Jordão <DUQ0288451@aluno.firjansenaisesi.com.br>
2023-12-07 11:43:44 -0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,38 @@
+/* Texto Giratório / Dark Mode */ 
+
+const text = document.querySelector(".atual-mode p")
+
+text.innerHTML = text.innerText.split("").map(
+    (char, i) =>
+    `<span style="transform:rotate(${i * 15}deg)">${char}</span>`
+).join("");
+
+
+
+
+const toggle = document.getElementById("checkbox");
+
+toggle.addEventListener('click', () => {
+    let oldTheme = localStorage.getItem("themeSystem") || "light";
+    let newTheme = oldTheme == "light" ? "dark" : "light";
+
+    localStorage.setItem("themeSystem", newTheme)
+    defineCurrentTheme(newTheme)
+})
+
+const themeSystem = localStorage.getItem("themeSystem") || "light"
+
+function defineCurrentTheme(theme){
+
+    document.documentElement.setAttribute("data-theme", theme)
+
+};
+
+if (themeSystem === "dark") {
+    text.innerText('modo escuro . modo escuro.')
+}
+
+
+toggle.onclick = function() {
+  let bb = 'a'  
+};
