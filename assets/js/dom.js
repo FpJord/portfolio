f5c793202a513bd4be645a3c9336118341a7a814
Felipe de Lima Jordão <DUQ0288451@aluno.firjansenaisesi.com.br>
2023-12-14 11:52:26 -0300

    semi feito

--- assets/js/dom.js
+++ assets/js/dom.js
@@ -1,42 +1,66 @@
 let listaProjetos = [
     {
         "nome": 'Kreativ Form - Clone',
+        "src": 'assets/css/kreativ-static.png',
         "stack 1": '<button class="html-css"> HTML/CSS </button>',
         "stack 2": '<button class="js"> JavaScript </button>',
         "img": '<div class="project-image" id="kreativ-project"></div>',
         "descrição": 'Projeto feito em 09/23 durante o curso Desenvolvedor Front-End no Senai Duque de Caxias. Feito com base no formulário de login do GitHub e adicionado um botão no canto superior direito para alterar o tema de Light para Dark.',
-        "deploy": '<button class="deploy"> Deploy </button>',
+        "deploy": 'https://kreativ-clone.vercel.app/',
     },
 
     {
         "nome": 'Calculadora IMC',
+        "src": 'assets/css/calculadora-static.png',
         "stack 1": '<button class="html-css"> HTML/CSS </button>',
         "stack 2": '<button class="js"> JavaScript </button>',
         "img": '<div class="project-image" id="calc-project"></div>',
         "descrição": 'Projeto feito em 09/23 durante o curso Desenvolvedor Front-End no Senai Duque de Caxias. Feito com base no formulário de login do GitHub e adicionado um botão no canto superior direito para alterar o tema de Light para Dark.',
-        "deploy": '<button class="deploy"> Deploy </button>',
+        "deploy": 'https://calculadora-imc-beige.vercel.app/',
     },
 
     {
         "nome": 'Sistok Landing Page',
+        "src": 'assets/css/sistok-static.png',
         "stack 1": '<button class="html-css"> HTML/CSS </button>',
         "stack 2": '<button class="ux-ui"> UX/UI </button>',
         "img": '<div class="project-image" id="sistok-project"></div>',
         "descrição": 'Projeto feito em 09/23 durante o curso Desenvolvedor Front-End no Senai Duque de Caxias. Feito com base no formulário de login do GitHub e adicionado um botão no canto superior direito para alterar o tema de Light para Dark.',
-        "deploy": '<button class="deploy"> Deploy </button>',
+        "deploy": 'https://sistok-clone-tau.vercel.app/',
     },
 
     {
         "nome": 'GitHub Clone',
+        "src": 'assets/css/github-static.png',
         "stack 1": '<button class="html-css"> HTML/CSS </button>',
         "stack 2": '<button class="js"> JavaScript </button>',
         "img": '<div class="project-image" id="github-project"></div>',
         "descrição": 'Projeto feito em 09/23 durante o curso Desenvolvedor Front-End no Senai Duque de Caxias. Feito com base no formulário de login do GitHub e adicionado um botão no canto superior direito para alterar o tema de Light para Dark.',
-        "deploy": '<button class="deploy"> Deploy </button>',
+        "deploy": 'https://github-clone-fp.vercel.app/',
+    },
+
+    {
+        "nome": 'OrbitShift Login',
+        "src": 'assets/css/orbitshift-static.png',
+        "stack 1": '<button class="html-css"> HTML/CSS </button>',
+        "stack 2": '<button class="js"> JavaScript </button>',
+        "img": '<div class="project-image" id="orbitshift-project"></div>',
+        "descrição": 'Projeto feito em 09/23 durante o curso Desenvolvedor Front-End no Senai Duque de Caxias. Feito com base no formulário de login do GitHub e adicionado um botão no canto superior direito para alterar o tema de Light para Dark.',
+        "deploy": 'https://orbitshift-fp.vercel.app/',
+    },
+
+    {
+        "nome": 'Skambo Login',
+        "src": 'assets/css/skambo-static.png',
+        "stack 1": '<button class="html-css"> HTML/CSS </button>',
+        "stack 2": '<button class="ux-ui"> UX/UI </button>',
+        "img": '<div class="project-image" id="skambo-project"></div>',
+        "descrição": 'Projeto feito em 09/23 durante o curso Desenvolvedor Front-End no Senai Duque de Caxias. Feito com base no formulário de login do GitHub e adicionado um botão no canto superior direito para alterar o tema de Light para Dark.',
+        "deploy": 'https://skambo-clone.vercel.app/',
     },
 ]
 
-listaProjetos.map((projeto) => {
+listaProjetos.map((projeto, posicao) => {
     let cardGrid = document.querySelector(".project-grid");
     let cardImage = document.querySelector(".project-image");
 
@@ -52,7 +76,22 @@ listaProjetos.map((projeto) => {
     
     ${projeto.img}
 
-    <button class="project-about">Sobre</button>
+    <button class="project-about" onclick="zoomProjeto('${posicao}')">Sobre</button>
 </div>
     `
-})
+
+    console.log(posicao)
+});
+
+function zoomProjeto(posicao){
+    let projetoSelecionado = listaProjetos[posicao];
+
+    document.getElementById("nomeProjeto").innerHTML = projetoSelecionado.nome;
+    document.getElementById("descricaoProjeto").innerHTML = projetoSelecionado.descrição;
+    document.getElementById("imgModal").src = projetoSelecionado.src;
+    document.getElementById("deploy-link").href = projetoSelecionado.deploy;
+
+
+    new bootstrap.Modal('#zoomProjeto').show();
+};
+
